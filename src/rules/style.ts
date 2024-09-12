fab4fc37585772c5fd0462e3c1eb1aba28571d2d
Marco Pasqualetti <marco.pasqualetti@live.com>
2024-09-12 09:43:08 +0200

    feat(rules/style): handle scenario with multiple imports and one satisfies the syntax option

--- src/rules/style.ts
+++ src/rules/style.ts
@@ -37,6 +37,12 @@ const styleRule: Rule.RuleModule = {
       StyleRuleOptions?
     ];
 
+    let acceptedImportSpecifier =
+      syntax === "default"
+        ? "ImportDefaultSpecifier"
+        : "ImportNamespaceSpecifier";
+    let hasAlreadyAnImportSatisfyingSyntax = false;
+
     const invalidImportTypes: Array<string> = [
       "ImportSpecifier",
       /**
@@ -49,19 +55,33 @@ const styleRule: Rule.RuleModule = {
     ];
 
     /**
-     * Variable used to store all react imports not following the required syntax
+     * @description Variable used to store all react import nodes
+     * not following the required syntax.
+     * Using an Array instead of a Set because I assume that there aren't many imports
      */
     const reactInvalidImports: Array<ESTree.ImportDeclaration> = [];
 
-    // Used to map react named imports to replace them with the original
+    /**
+     * @description Used to map react named imports to replace them with the original
+     */
     const reactNamedImports = new Map<string, string>();
 
     return {
       ImportDeclaration: (node) => {
-        const { source, specifiers } = node;
-
         /** @todo might change selector to something like ImportDeclaration[source.value="react"] */
-        if (source.value !== "react") return;
+        if (node.source.value !== "react") return;
+
+        const { specifiers } = node;
+
+        // if an import satisfies the preferred syntax it should not be added
+        // to invalid imports and should be remembered ti avoid
+        if (
+          specifiers.length === 1 &&
+          specifiers[0].type === acceptedImportSpecifier
+        ) {
+          hasAlreadyAnImportSatisfyingSyntax = true;
+          return;
+        }
 
         let hasAtLeastOneNamedImport = false;
         for (const specifier of specifiers) {
@@ -87,7 +107,7 @@ const styleRule: Rule.RuleModule = {
       /** Check all identifiers and if they match the one imported from React add the prefix */
       'Identifier[parent.type!="ImportDefaultSpecifier"][parent.type!="ImportSpecifier"][parent.type!="ImportNamespaceSpecifier"]':
         (node: ESTree.Identifier) => {
-          if (reactNamedImports.size > 0 && reactNamedImports.has(node.name)) {
+          if (reactNamedImports.has(node.name)) {
             const originalImportName = reactNamedImports.get(node.name)!;
 
             return context.report({
@@ -104,45 +124,46 @@ const styleRule: Rule.RuleModule = {
         /** Check if there is at least one invalid import */
         if (!reactInvalidImports.length) return;
 
-        const [firstImport, ...otherReactImports] = reactInvalidImports;
-
-        /** Replace the first import with the right import based on options */
-        context.report({
-          messageId: "wrongImport",
-          data: { syntax },
-          loc: { ...firstImport.loc! },
-          fix(fixer) {
-            /** Cycle all imports to understand if it should become a import type */
-            const importType = reactInvalidImports.every(
-              (importNode) =>
-                "importKind" in importNode && importNode.importKind === "type"
-            )
-              ? "type"
-              : "";
-
-            const correctImportSyntax =
-              syntax === "default" ? "React" : "* as React";
-
-            /** @todo maybe there is a more smart method working directly on AST node to do this */
-            const newImport = `import ${importType} ${correctImportSyntax} from 'react';`;
-
-            return fixer.replaceText(
-              firstImport,
-              newImport.replace(/\s+/g, " ")
-            );
-          },
-        });
-
         /** All additional imports can be removed */
-        for (const reactImportNode of otherReactImports) {
-          context.report({
-            messageId: "multipleImport",
-            data: { syntax },
-            loc: { ...reactImportNode.loc! },
-            fix(fixer) {
-              return fixer.remove(reactImportNode);
-            },
-          });
+        for (const [index, reactImportNode] of reactInvalidImports.entries()) {
+          if (!hasAlreadyAnImportSatisfyingSyntax && index === 0) {
+            /** Replace the first import with the right import based on options */
+            context.report({
+              messageId: "wrongImport",
+              data: { syntax },
+              loc: { ...reactImportNode.loc! },
+              fix(fixer) {
+                /** Cycle all imports to understand if it should become a import type */
+                const importType = reactInvalidImports.every(
+                  (importNode) =>
+                    "importKind" in importNode &&
+                    importNode.importKind === "type"
+                )
+                  ? "type"
+                  : "";
+
+                const correctImportSyntax =
+                  syntax === "default" ? "React" : "* as React";
+
+                /** @todo maybe there is a more smart method working directly on AST node to do this */
+                const newImport = `import ${importType} ${correctImportSyntax} from 'react';`;
+
+                return fixer.replaceText(
+                  reactImportNode,
+                  newImport.replace(/\s+/g, " ")
+                );
+              },
+            });
+          } else {
+            context.report({
+              messageId: "multipleImport",
+              data: { syntax },
+              loc: { ...reactImportNode.loc! },
+              fix(fixer) {
+                return fixer.remove(reactImportNode);
+              },
+            });
+          }
         }
       },
     };
